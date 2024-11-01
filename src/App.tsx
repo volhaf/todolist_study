329261a9676a1d09bebf3a52afbd0bf2c6e92785
volhaf <Volha.f@outlook.com>
2024-11-01 15:56:04 +0100

    add AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { AddItemForm } from './AddItemForm';
 import './App.css';
 import {Todolist} from "./Todolist";
@@ -122,6 +124,26 @@ function App() {
 
 	return (
 		<div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon/>
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            TODOLIST
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+
+
+
 			<AddItemForm addItem={addTodoList}/>
 
 			{todolists.map((tl) => {
